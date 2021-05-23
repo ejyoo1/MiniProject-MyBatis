dbb92ccf425b1f51dce1b3f273595faafa67cb3f
ejyoo1 <o3k3585@gmail.com>
2021-05-23 18:54:02 +0900

    공지사항 VO 생성

---
+++ MiniProject_mybatis/src/ejyoo/dto/NoticeVO.java
@@ -0,0 +1,57 @@
+package ejyoo.dto;
+
+public class NoticeVO {
+	private String noticeNo;
+	private String noticeTitle;
+	private String noticeContents;
+	private String noticeWriter;
+	private String noticeEditer;
+	private String noticeCreate;
+	private String noticeModify;
+	
+	public String getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(String noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	public String getNoticeContents() {
+		return noticeContents;
+	}
+	public void setNoticeContents(String noticeContents) {
+		this.noticeContents = noticeContents;
+	}
+	public String getNoticeWriter() {
+		return noticeWriter;
+	}
+	public void setNoticeWriter(String noticeWriter) {
+		this.noticeWriter = noticeWriter;
+	}
+	public String getNoticeEditer() {
+		return noticeEditer;
+	}
+	public void setNoticeEditer(String noticeEditer) {
+		this.noticeEditer = noticeEditer;
+	}
+	public String getNoticeCreate() {
+		return noticeCreate;
+	}
+	public void setNoticeCreate(String noticeCreate) {
+		this.noticeCreate = noticeCreate;
+	}
+	public String getNoticeModify() {
+		return noticeModify;
+	}
+	public void setNoticeModify(String noticeModify) {
+		this.noticeModify = noticeModify;
+	}
+	
+	
+
+}
