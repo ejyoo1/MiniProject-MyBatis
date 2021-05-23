45ad63c7b7e80a1da4465c197f9b7816a68cee68
ejyoo1 <o3k3585@gmail.com>
2021-05-23 18:59:23 +0900

    Notice Query 클래스 생성

---
+++ MiniProject_mybatis/test/kr/or/ddit/mybatis/TestOracleMyBatisNoticeQuery.java
@@ -0,0 +1,9 @@
+package kr.or.ddit.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestOracleMyBatisNoticeQuery {
+}
