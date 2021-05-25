1a41defe5ed1065ffafa20123c6624615a8cc318
ejyoo1 <o3k3585@gmail.com>
2021-05-25 18:22:18 +0900

    no message

--- MiniProject_mybatis/src/ejyoo/service/MockNoticeDAO.java
+++ MiniProject_mybatis/src/ejyoo/service/MockNoticeDAO.java
@@ -1,6 +1,7 @@
 package ejyoo.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,12 +17,26 @@ public class MockNoticeDAO implements INoticeDAO{
 		
 		if(session.getConnection() == null) throw new SQLException();
 		
+		NoticeVO notice = new NoticeVO();
+		notice.setNoticeNo("20");
+		
+		noticeList = new ArrayList<NoticeVO>();
+		noticeList.add(notice);
+		
 		return noticeList;
 	}
 
 	@Override
 	public List<NoticeVO> selectNoticeByInfo(SqlSession session, NoticeVO noticeVo) throws SQLException {
 		List<NoticeVO> noticeList = null;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		
+		NoticeVO notice = new NoticeVO();
+		notice.setNoticeNo("20");
+		
+		noticeList = new ArrayList<NoticeVO>();
+		noticeList.add(notice);
 		
 		return noticeList;
 	}
@@ -30,12 +45,20 @@ public class MockNoticeDAO implements INoticeDAO{
 	public NoticeVO selectNoticeByNo(SqlSession session, String noticeNo) throws SQLException {
 		NoticeVO notice = null;
 		
+		if(session.getConnection() == null) throw new SQLException();
+		
+		notice = new NoticeVO();
+		notice.setNoticeNo("20");
+		
 		return notice;
 	}
 
 	@Override
 	public int insertNoticeByInfo(SqlSession session, NoticeVO noticeVo) throws SQLException {
 		int cnt = 0;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		cnt = 1;
 		
 		return cnt;
 	}
@@ -44,12 +67,18 @@ public class MockNoticeDAO implements INoticeDAO{
 	public int updateNoticeByInfo(SqlSession session, NoticeVO noticeVo) throws SQLException {
 		int cnt = 0;
 		
+		if(session.getConnection() == null) throw new SQLException();
+		cnt = 1;
+		
 		return cnt;
 	}
 
 	@Override
 	public int deleteNoticeByNo(SqlSession session, String noticeNo) throws SQLException {
 		int cnt = 0;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		cnt = 1;
 		
 		return cnt;
 	}
--- MiniProject_mybatis/src/ejyoo/service/TestNoticeService.java
+++ MiniProject_mybatis/src/ejyoo/service/TestNoticeService.java
@@ -1,9 +1,12 @@
 package ejyoo.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import ejyoo.dto.NoticeVO;
@@ -19,14 +22,59 @@ public class TestNoticeService {
 	}
 	
 	@Test
-	public void Test1_getNoticeList() throws Exception{
-		List<NoticeVO>  
+	public void Test1_getNoticeList() throws SQLException {
+		List<NoticeVO> noticeList = service.getNoticeList();
+		
+		Assert.assertEquals(1, noticeList.size());
+		Assert.assertEquals("20", noticeList.get(0).getNoticeNo());
 		
 	}
-	List<NoticeVO> getNoticeByInfo(NoticeVO noticeVo)
-	NoticeVO getNoticeByNo(String noticeNo)
-	int insertNoticeByInfo(NoticeVO noticeVo)
-	int updateNoticeByInfo(NoticeVO noticeVo)
-	int deleteNoticeByNo(String noticeNo)
 	
+	@Test
+	public void Test2_getNoticeByInfo() throws SQLException {
+		NoticeVO noticeVo = new NoticeVO();
+		List<NoticeVO> noticeList = service.getNoticeByInfo(noticeVo);
+		
+		Assert.assertEquals(1, noticeList.size());
+		Assert.assertEquals("20", noticeList.get(0).getNoticeNo());
+	}
+	
+	@Test
+	public void Test3_getNoticeByNo() throws SQLException {
+		NoticeVO noticeVo = new NoticeVO();
+		List<NoticeVO> noticeList = service.getNoticeByInfo(noticeVo);
+		
+		Assert.assertEquals(1, noticeList.size());
+		Assert.assertEquals("20", noticeList.get(0).getNoticeNo());
+	}
+	
+	@Test
+	public void Test4_insertNoticeByInfo() throws SQLException {
+		int cnt = 0;
+		NoticeVO noticeVo = new NoticeVO();
+		
+		cnt = service.insertNoticeByInfo(noticeVo);
+		
+		Assert.assertEquals(1, cnt);
+	}
+	
+	@Test
+	public void Test5_updateNoticeByInfo() throws SQLException {
+		int cnt = 0;
+		NoticeVO noticeVo = new NoticeVO();
+		
+		cnt = service.insertNoticeByInfo(noticeVo);
+		
+		Assert.assertEquals(1, cnt);
+	}
+	
+	@Test
+	public void Test6_deleteNoticeByNo() throws SQLException {
+		int cnt = 0;
+		NoticeVO noticeVo = new NoticeVO();
+		
+		cnt = service.insertNoticeByInfo(noticeVo);
+		
+		Assert.assertEquals(1, cnt);
+	}
 }
